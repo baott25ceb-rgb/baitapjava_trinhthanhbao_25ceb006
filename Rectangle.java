c5fc6eff062be7c3a99ffa96c3e77c8947fd6d22
baott25ceb-rgb <baott.25ceb@vku.udn.vn>
2025-11-06 00:04:38 +0700

    Bai4

--- Rectangle.java
+++ Rectangle.java
@@ -1,31 +1,48 @@
-public class Rectangle {
-    private float length = 1.0f;
-    private float width = 1.0f;
-public Rectangle(){
-}
-public Rectangle(float length, float width){
-    this.length = length;
-    this.width = width;
-}
-public float getLength() {
-    return length;
-}
-public void setLength(float length){
-    this.length = length;
-}
-public float getWidth(){
-    return width;
-}
-public void setWidth(float width) {
-    this.width = width;
-}
-public double getArea(){
-    return length*width;
-}
-public double getPerimeter(){
-    return 2*(length+width);
-}public String toString(){
-        return "Rectangle[length ="+length+ "width=" + width + "]";
-    }
-}
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double length = 1.0;
 
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle [Shape"+super.toString()+", width= "+width+ ", length= "+length+"]";
+    }
+
+}
