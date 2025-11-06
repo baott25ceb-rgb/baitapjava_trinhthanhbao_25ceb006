a447c0a5bef687369078700728f9ac57a785e496
baott25ceb-rgb <baott.25ceb@vku.udn.vn>
2025-11-06 07:52:04 +0700

    Bai4.6

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog extends Mammal{
+    public Dog(String name){
+        super(name);
+    }
+    public void greets(){
+        System.out.println("Woof");
+    }
+    public void greets(Dog anotherDog){
+        System.out.println("Woooof");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog ["+super.toString()+"]";
+    }
+}
